34664602d77516de702c992e38ddaa220efc5d1d
Chotuni <94113055+Chotuni@users.noreply.github.com>
2026-03-27 12:30:05 +0100

    Add files via upload

---
+++ Assets/Scripts/AjedrezLogica/Pieza.cs
@@ -0,0 +1,13 @@
+﻿using AjedrezLogica.Recursos;
+
+namespace AjedrezLogica
+{
+    public class Pieza
+    {
+        public TipoPieza Tipo { get; set; }
+        public ColorPieza Color { get; set; }
+
+        public (int X, int Y) Posicion { get; set; }
+
+    }
+}
