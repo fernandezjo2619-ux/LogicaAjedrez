844c87bcfccf962f49a33328841712faac721058
samperlop2006-alt <samperlop2006@gmail.com>
2026-04-12 10:43:22 +0200

    scrips dirigidos para pruebas sin unity

---
+++ Assets/Scripts/AjedrezLogica/Base de datos/No Unity/Sacar datos/DatosHabilidad.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace SupabaseAjedrez
+{
+    public class DatosHabilidad
+    {
+        public int id_habilidad { get; set; }
+        public string nombre_habilidad { get; set; }
+        public string descripcion { get; set; }
+        public int poder_base { get; set; }
+    }
+}
